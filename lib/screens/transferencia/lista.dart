459c2bca264aea9d6bd835dbbda44738ef7b3fc1
gdarlan <gabriel.darlan@hotmail.com>
2019-10-06 14:25:31 -0300

    Projeto do curso um finalizado

---
+++ lib/screens/transferencia/lista.dart
@@ -0,0 +1,68 @@
+import 'package:bytebank/models/transferencia.dart';
+import 'package:flutter/material.dart';
+
+import 'formulario.dart';
+
+const _tituloAppBar = 'Transferência';
+
+class ListaTransferencias extends StatefulWidget {
+  final List<Transferencia> _transferencia = List();
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return ListaTransferenciaState();
+  }
+}
+
+class ListaTransferenciaState extends State<ListaTransferencias> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_tituloAppBar),
+      ),
+      body: ListView.builder(
+        itemCount: widget._transferencia.length,
+        itemBuilder: (context, indice) {
+          final transferencia = widget._transferencia[indice];
+          return ItemTransferencia(transferencia);
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          final Future<Transferencia> future =
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
+            return FormularioTransferencia();
+          }));
+          future.then((transferenciaRecebida) {
+            if (transferenciaRecebida != null) {
+              setState(() {
+                widget._transferencia.add(transferenciaRecebida);
+              });
+            }
+          });
+        },
+      ),
+    );
+  }
+}
+
+class ItemTransferencia extends StatelessWidget {
+  final Transferencia _transferencia;
+
+  ItemTransferencia(this._transferencia);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text(_transferencia.valor.toString()),
+        subtitle: Text(_transferencia.numeroConta.toString()),
+      ),
+    );
+  }
+}
